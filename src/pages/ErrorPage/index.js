32dce1f03df43d3002d747f17c2d17414ad25b1c
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2022-12-31 19:11:54 +0200

    replace logo with vector version

--- src/pages/ErrorPage/index.js
+++ src/pages/ErrorPage/index.js
@@ -1,5 +1,6 @@
 import { useRouteError } from "react-router-dom";
 import {Link} from "react-router-dom"; 
+import img from "../../assets/racehunter-logo.svg";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -7,7 +8,7 @@ export default function ErrorPage() {
 //αυτο ειναι το error page, γενικά μπορεί να αλλάξει όλο το style του αν κάποιος το κρίνει απαραίτητο 
   return (
     <div id="error-page">
-      <Link to="/"><img src={require("../../assets/RaceHunter_Gradient_Logo.png")} /></Link>
+      <Link to="/"><img src={img} /></Link>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i> 
