374028e0be3b420972129a2d1f99a93ee0958f84
lnesh <107777140+lnesh@users.noreply.github.com>
2023-01-05 21:20:25 +0200

    Css changes for homepage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Hero from './assets/banner-img.png'
 import Feat1 from  './assets/feat1.png';
 import Feat2 from  './assets/feat2.png';
 import Feat3 from  './assets/feat3.png';
-import RacesPage from "./pages/RacesPage";
+// import RacesPage from "./pages/RacesPage";
 import './assets/styles.css';
 
 
@@ -38,8 +38,8 @@ export default function App() {
 
            <section className='features'>
               <div className='feature1'>
-                 <span id='description'><h1 id='header' >Όλες οι πληροφορίες για<br></br> τον αγώνα</h1>
-                 <p id='info'>  Όλες οι πληροφορίες που θα χρειαστείς οργανωμένες σε ένα σημείο για άμεση και εύκολη πρόσβαση </p>
+                 <span className='description'><h1 className='header' >Όλες οι πληροφορίες για<br></br> τον αγώνα</h1>
+                 <p className='info'>Όλες οι πληροφορίες που θα χρειαστείς οργανωμένες σε ένα σημείο για άμεση και εύκολη πρόσβαση</p>
                  </span>
                  <img src={Feat1} id='feat1-img'/>
                  
@@ -48,17 +48,17 @@ export default function App() {
 
               <div className='feature2'>
                 <img src={Feat2} id='feat2-img'/>
-                <span id='description'><h1 id='header'>Φίλτρα αναζήτησης</h1>
-                <p id='info'>Βρες αγώνες με βάση κριτήρια, όπως η απόσταση, το κόστος συμμετοχής, ο τύπος της διαδρομής και πολλά άλλα</p>
+                <span className='description'><h1 className='header'>Φίλτρα αναζήτησης</h1>
+                <p className='info'>Βρες αγώνες με βάση κριτήρια, όπως η απόσταση, το κόστος συμμετοχής, ο τύπος της διαδρομής και πολλά άλλα</p>
                 </span>
                 
               </div>
 
               <div className='feature3'>
                
-               <span id='description'>
-                  <h1 id='header'>Μοναδικές προσφορές</h1>
-                  <p id='info'>Απόκτησε πρόσβαση σε πληθώρα εκπτώσεων και προσφορών για τη συμμετοχή σου σε αγώνες, την αγορά εξοπλισμού, την ενοικίαση καταλυμάτων κ.α.</p>
+               <span className='description'>
+                  <h1 className='header'>Μοναδικές προσφορές</h1>
+                  <p className='info'>Απόκτησε πρόσβαση σε πληθώρα εκπτώσεων και προσφορών για τη συμμετοχή σου σε αγώνες, την αγορά εξοπλισμού, την ενοικίαση καταλυμάτων κ.α.</p>
                 </span>
                 <img src={Feat3} id='feat3-img'/> 
               </div>
