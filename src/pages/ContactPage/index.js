cddbea6f003f2221ade5b090a8de1c8af45129d2
TolisSth <tolishalis@gmail.com>
2023-02-21 20:35:27 +0200

    Requested changes made

--- src/pages/ContactPage/index.js
+++ src/pages/ContactPage/index.js
@@ -1,6 +1,6 @@
 import React from "react"; 
-import { AiOutlineMail } from "react-icons/ai"; 
-import { HiOutlineLocationMarker } from "react-icons/hi"; 
+import { HiMail } from "react-icons/hi"; 
+import { HiLocationMarker } from "react-icons/hi";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import "./contact-page.css";
@@ -9,6 +9,11 @@ export default function ContactPage(){
     return (
         <div>
             <Navbar />
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+
                 <div className="background-contact">
                     <h1 id="title-of-background">Επικοινωνήστε μαζί μας</h1>
                     <div className="contact-card">
@@ -23,11 +28,12 @@ export default function ContactPage(){
                             <br></br>
                             <input id="send" type="submit" value="Αποστολή"></input>
                         </form>
-                        <div className="contact-info">
-                            <h2><HiOutlineLocationMarker />  Eγνατία 156, 546 36,<br></br>Θεσσαλονίκη, Ελλάδα</h2>
-                            <br></br>
-                            <h2><AiOutlineMail />  contact@racehunter.gr</h2>
-                        </div>
+                            <div className="contact-info-grid">
+                                <h1 className="marker" id="location-marker"><HiLocationMarker /></h1> 
+                                <h2 id="addr">Eγνατία 156, 546 36,<br></br>Θεσσαλονίκη, Ελλάδα</h2>
+                                <h1 className="marker" id="mail-marker"><HiMail /></h1>
+                                <h2 id="email">  contact@racehunter.gr</h2>
+                            </div>
                     </div>
                 </div>
             <Footer />
