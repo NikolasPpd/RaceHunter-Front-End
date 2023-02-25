15c1ae56b907cef7af95fde38701c7f4f13c9002
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2023-02-25 20:39:35 +0200

    Replace checkboxes in Amenities Filters component

--- src/pages/RacesPage/Filters/AmenitiesFilters/index.js
+++ src/pages/RacesPage/Filters/AmenitiesFilters/index.js
@@ -1,91 +1,76 @@
 import React, { useState } from 'react';
-// import ReactDOM from 'react-dom/client';
-import "./amenities-filters.css"
+import './amenities-filters.css'
+import CustomCheckbox from '../../../../components/CustomCheckbox';
 
 // Rename το function και όλες τις σχετικές μεταβλητές σε "AmenitiesFilters"
-export default function RaceAmenities(){
+export default function AmenitiesFilters(){
+  
+  //Race amenities
+  const [raceAmenities,setRaceAmenities] = useState({
+    troph:false,
+    medals:false,
+    shirt:false,
+    results:false,
+    onlinePayment:false
+  });
 
+  //Number of results for each race category
+  const[aidStationResultsCount,setAidStationResultsCount] = useState(0)
+  const[medalResultsCount,setMedalResultsCount] = useState(0)
+  const[shirtResultsCount,setShirtResultsCount] = useState(0)
+  const[resultsResultsCount,setResultsResultsCount] = useState(0)
+  const[onlinePaymentResultsCount,setOnlinePaymentResultsCount] = useState(0)
 
- //States for each marathon
- const[troph,settroph]=useState(0)
- const[medals,setmedals]=useState(0)
- const[shirt,setshirt]=useState(0)
- const[results,setresults]=useState(0)
- const[onlinepay,setonlinepay]=useState(0)
+  const Handling = (name) => {
+    const checked = !raceAmenities[name];
+    setRaceAmenities(raceAmenities => ({
+      ...raceAmenities,
+      [name] : checked
+    }));
+  };
 
-
-//Marathon's race amenities [Παροχές αγώνα Μαραθωνίων]
-const [RaceAmenities,setRaceAmenities] = useState({troph:false,medals:false,
-    shirt:false,results:false,onlinepay:false})
-
-//Marathon's RaceAmenities Event Handling
-function HandlingAmenities(event){
-    const {name,type,value,checked} = event.target
-    setRaceAmenities(AmenitiesData => {
-            return{
-                ...AmenitiesData ,[name] : type === "checkbox" ? checked : value
-            }
-    })
-
-}
-
-    return (
-
-      // Γιατί δεν υπάρχει form?
-        <div className='raceAmerForm'>
-            <p>Παροχές Αγώνα</p>
-            <ul>
-              <li>
-              <input
-            type = "checkbox"
-            id="Troph"
-            checked = {RaceAmenities.troph}
-            onChange={HandlingAmenities}
-            name = "troph"
-          />
-          <label>Τροφοδοσία ({troph})</label>
-              </li>
-              <li>
-              <input
-            type = "checkbox"
-            id="Medals"
-            checked = {RaceAmenities.medals}
-            onChange={HandlingAmenities}
-            name = "medals"
-          />
-          <label>Μετάλλια ({medals})</label>
-              </li>
-              <li>
-              <input
-            type = "checkbox"
-            id="Shirt"
-            checked = {RaceAmenities.shirt}
-            onChange={HandlingAmenities}
-            name = "shirt"
-          />
-          <label>Μπλούζα ({shirt})</label>
-              </li>
-              <li>
-              <input
-            type = "checkbox"
-            id="Results"
-            checked = {RaceAmenities.results}
-            onChange={HandlingAmenities}
-            name = "results"
-          />
-          <label>Αποτελέσματα ({results})</label>
-              </li>
-              <li>
-              <input
-            type = "checkbox"
-            id="Onlinepay"
-            checked = {RaceAmenities.onlinepay}
-            onChange={HandlingAmenities}
-            name = "onlinepay"
-          />
-          <label>Online πληρωμή ({onlinepay})</label>
-              </li>
-            </ul>  
-        </div>
-    )
+  return (
+    <div>
+        <form className='race-amenities-form'>
+            <p>Μήκος Διαδρομής</p>
+            <div className='race-amenities-elements'>
+              <CustomCheckbox
+                label='Τροφοδοσία'
+                count={aidStationResultsCount}
+                initiallyChecked={raceAmenities.aidStation}
+                onChange={Handling}
+                name='aidStation'
+              />
+              <CustomCheckbox
+                label='Μετάλλια'
+                count={medalResultsCount}
+                initiallyChecked={raceAmenities.medals}
+                onChange={Handling}
+                name='medals'
+              />
+              <CustomCheckbox
+                label='Μπλούζα'
+                count={shirtResultsCount}
+                initiallyChecked={raceAmenities.shirt}
+                onChange={Handling}
+                name='shirt'
+              />
+              <CustomCheckbox
+                label='Αποτελέσματα'
+                count={resultsResultsCount}
+                initiallyChecked={raceAmenities.results}
+                onChange={Handling}
+                name='results'
+              />
+              <CustomCheckbox
+                label='Online πληρωμή'
+                count={onlinePaymentResultsCount}
+                initiallyChecked={raceAmenities.onlinePayment}
+                onChange={Handling}
+                name='onlinePayment'
+              />
+            </div>
+        </form>
+    </div>
+  );
 }
