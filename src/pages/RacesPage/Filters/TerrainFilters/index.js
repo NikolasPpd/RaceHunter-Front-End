e13e0b5d2c9ae754625d524bcbd363e029d11cfa
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2023-02-25 20:39:25 +0200

    Replace checkboxes in Terrain Filters component

--- src/pages/RacesPage/Filters/TerrainFilters/index.js
+++ src/pages/RacesPage/Filters/TerrainFilters/index.js
@@ -1,92 +1,76 @@
-import React, { useState } from "react";
-import "./terrain-filters.css";
+import React, { useState } from 'react';
+import './terrain-filters.css';
+import CustomCheckbox from '../../../../components/CustomCheckbox';
 
 // Rename το function και όλες τις σχετικές μεταβλητές σε "TerrainFilters"
-export default function Distance(){
+export default function TerrainFilters() {
 
+  //Race terrain types
+  const [groundTypeData,setGroundTypeData] = useState({
+    asphalt: false,
+    mountain: false,
+    grass: false,
+    sand: false,
+    multiTerrain: false
+  });
 
- //States for each marathon
- const[asphalt,setasphalt]=useState(0)
- const[mountain,setmountain]=useState(0)
- const[grass,setgrass]=useState(0)
- const[sand,setsand]=useState(0)
- const[multiter,setmultiter]=useState(0)
+  //Number of results for each race category
+  const[asphaltResultsCount,setAsphaltResultsCount] = useState(0)
+  const[mountainResultsCount,setMountainResultsCount] = useState(0)
+  const[grassResultsCount,setGrassResultsCount] = useState(0)
+  const[sandResultsCount,setSandResultsCount] = useState(0)
+  const[multiTerrainResultsCount,setMultiTerrainResultsCount] = useState(0)
 
+  const Handling = (name) => {
+    const checked = !groundTypeData[name];
+    setGroundTypeData(groundTypeData => ({
+      ...groundTypeData,
+      [name] : checked
+    }));
+  };
 
-//Marathon's ground type [Είδος ασφάλτου Μαραθωνίων]
-const [GroundTypeData,setGroundTypeData] = useState({asphalt:false,mountain:false,
-    grass:false,sand:false,multiter:false})
-
-//Marathon's distance Event Handling
-function HandlingGround(event){
-    const {name,type,value,checked} = event.target
-    setGroundTypeData(GroundData => {
-            return{
-                ...GroundData ,[name] : type === "checkbox" ? checked : value
-            }
-    })
-
-}
-
-
-    return (
-
-      // Γιατί δεν υπάρχει form?
-        <div className='groundForm'>
+  return (
+    <div>
+        <form className='race-terrain-form'>
             <p>Έδαφος Διαδρομής</p>
-            <ul>
-              <li>
-              <input
-            type = "checkbox"
-            id="Asphalt"
-            checked = {GroundTypeData.asphalt}
-            onChange={HandlingGround}
-            name = "asphalt"
-          />
-          <label>Άσφαλτος ({asphalt})</label>
-              </li>
-              <li>
-              <input
-            type = "checkbox"
-            id="Mountain"
-            checked = {GroundTypeData.mountain}
-            onChange={HandlingGround}
-            name = "mountain"
-          />
-          <label>Βουνό ({mountain})</label>
-              </li>
-              <li>
-              <input
-            type = "checkbox"
-            id="Grass"
-            checked = {GroundTypeData.grass}
-            onChange={HandlingGround}
-            name = "grass"
-          />
-          <label>Γρασίδι ({grass})</label>
-              </li>
-              <li>
-              <input
-            type = "checkbox"
-            id="Sand"
-            checked = {GroundTypeData.sand}
-            onChange={HandlingGround}
-            name = "sand"
-          />
-          <label>Άμμος ({sand})</label> 
-              </li>
-              <li>
-              <input
-            type = "checkbox"
-            id="Multiterrain"
-            checked = {GroundTypeData.multiter}
-            onChange={HandlingGround}
-            name = "multiter"
-          />
-          <label>Multi-terrain ({multiter})</label>
-              </li>
-            </ul>   
-        </div>
-        
-    )
+            <div className='race-terrain-elements'>
+              <CustomCheckbox
+                label='Άσφαλτος'
+                count={asphaltResultsCount}
+                initiallyChecked={groundTypeData.asphalt}
+                onChange={Handling}
+                name='asphalt'
+              />
+              <CustomCheckbox
+                label='Βουνό'
+                count={mountainResultsCount}
+                initiallyChecked={groundTypeData.mountain}
+                onChange={Handling}
+                name='mountain'
+              />
+              <CustomCheckbox
+                label='Γρασίδι'
+                count={grassResultsCount}
+                initiallyChecked={groundTypeData.grass}
+                onChange={Handling}
+                name='grass'
+              />
+              <CustomCheckbox
+                label='Άμμος'
+                count={sandResultsCount}
+                initiallyChecked={groundTypeData.sand}
+                onChange={Handling}
+                name='sand'
+              />
+              <CustomCheckbox
+                label='Multi-terrain'
+                count={multiTerrainResultsCount}
+                initiallyChecked={groundTypeData.multiTerrain}
+                onChange={Handling}
+                name='multiTerrain'
+              />
+            </div>
+        </form>
+    </div>
+  );
 }
