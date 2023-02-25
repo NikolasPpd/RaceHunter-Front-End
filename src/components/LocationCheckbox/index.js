59841449db3afca510662d45698a99a1d9769e51
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2023-02-25 23:52:28 +0200

    Create location checkbox component

---
+++ src/components/LocationCheckbox/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { MdOutlineGpsFixed } from "react-icons/md";
+import './location-checkbox.css';
+
+export default function LocationCheckbox({ label, initialChecked: initiallyChecked = false, onChange, name }) {
+    const defaultChecked = initiallyChecked ? true : false;
+    const [isChecked, setIsChecked] = useState(defaultChecked);
+
+    const handleClick = () => {
+        if (!isChecked) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const { latitude, longitude } = position.coords;
+                console.log(latitude, longitude);
+            })
+        }
+        setIsChecked(prevIsChecked => !prevIsChecked);
+        onChange(name);
+    };
+
+    return (
+        <div className='location-checkbox-container' onClick={handleClick}>
+            <MdOutlineGpsFixed size={22}/>
+            <span>{label}</span>
+            <input type="checkbox" checked={isChecked} readOnly
+            className={isChecked ? 'checked' : ''} />
+        </div>
+    );
+}
